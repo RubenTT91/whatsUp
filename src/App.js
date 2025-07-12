7671e87ff0f0e970612190f0cf05b6c0bca29a3d
Corporativo <Ruben.toro1@gmail.com>
2025-07-12 11:37:09 -0500

    Button goto, created

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import First_section from './components/sections_page/first_section.jsx';
-import Second_Section from './components/sections_page/second_section.jsx';
-import NavBar from './components/navBar/navBar.jsx';
-import backgroundImg from'./img/background.jpg'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import First_section from "./components/sections_page/first_section.jsx";
+import Second_Section from "./components/sections_page/second_section.jsx";
+import NavBar from "./components/navBar/navBar.jsx";
+import ToHome from "./components/others_commponents/To_Home.jsx";
 
 function App() {
   return (
-   <>
-   <div className="bodyMenu">
-    {/* <img src={backgroundImg}/> */}
-    <NavBar />     
-   <First_section/>
-   <Second_Section/>
-   </div>   
-   </>
+    <>
+      <ToHome/>
+      <NavBar />
+      <First_section />
+      <Second_Section />
+    </>
   );
 }
 
--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -1,52 +1,50 @@
 import React from "react";
 import First_section from "../sections_page/first_section";
 import Second_Section from "../sections_page/second_section";
-import { Link } from "react-scroll";
+import { Link, Element } from "react-scroll";
 
 function NavBar() {
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            Mi Portafolio
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon">0</span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
+      <Element name="home">
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">
+              Mi Portafolio
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon">0</span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav">
                 <Link to="section1" smooth={true} duration={500}>
-              <li className="nav-item">
-                <a
-                  className="nav-link active"
-                  aria-current="page"
-                  href={<First_section />}
-                >
-                  ¿Quien soy?
-                </a>
-              </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">
+                      ¿Quien soy?
+                    </a>
+                  </li>
                 </Link>
-              <li className="nav-item">
-                <a className="nav-link" href={<Second_Section />}>
-                  Mis Proyectos
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#"></a>
-              </li>
-            </ul>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Mis Proyectos
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#"></a>
+                </li>
+              </ul>
+            </div>
           </div>
-        </div>
-      </nav>
+        </nav>
+      </Element>
     </>
   );
 }
