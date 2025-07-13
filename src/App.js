a7939ce7bc347fbb69b96fe728e94ede2f510e97
Corporativo <Ruben.toro1@gmail.com>
2025-07-13 16:37:42 -0500

    Third rock, added.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import NavBar from "./components/navBar/navBar.jsx";
 import ToHome from "./components/others_commponents/To_Home.jsx";
 import g21 from "./img/bk/bk_r1/g21.png";
 import g22 from "./img/bk/bk_r1/g22.png";
+import g23 from "./img/bk/bk_r1/g23.png";
 
 function App() {
   return (
@@ -13,13 +14,14 @@ function App() {
 
       <img className="br" src={g21} />
       <img className="br2" src={g22} />
+      <img className="br3" src={g23} />
       <ToHome />
       <NavBar />
       <First_section />
       <Second_Section />
     <div className="background"> 
     </div>
-    </>
+    </> 
   );
 }
 
